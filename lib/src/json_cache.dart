b4db1e479629360c25fa7b116938c23e5b25b6d9
Rafael Amizes <r.amizes@gmail.com>
2021-08-21 19:36:28 -0300

    fix grammar errors

--- lib/src/json_cache.dart
+++ lib/src/json_cache.dart
@@ -3,11 +3,11 @@ abstract class JsonCache {
   /// Frees up storage space.
   Future<void> clear();
 
-  /// Updates data at [key], or creates a new cache line at [key] if there is no
+  /// Updates data at [key] or creates a new cache line at [key] if there is no
   /// previous data there.
   Future<void> refresh(String key, Map<String, dynamic> data);
 
-  /// Removes data from cache at [key] and returns it, or returns null if there
+  /// Removes data from cache at [key] and returns it or returns null if there
   /// is no data at [key].
   Future<Map<String, dynamic>?> erase(String key);
 
