326f2385ccd2e6508734cbc4199c2bbdb818414a
Rafael Amizes <r.amizes@gmail.com>
2021-08-25 12:23:04 -0300

    style: pub dev fixes
    * refactor! rename JsonCacheMem.mem to JsonCacheMem.ext
    
    * refactor!: revert JsonCacheMem.mem constructor
    
    * style: pub dev fixes
    
    * style: run flutter format
    
    * refactor: more efficient refresh impl.

--- lib/src/json_cache.dart
+++ lib/src/json_cache.dart
@@ -1,4 +1,4 @@
-/// Represents data cached in json.
+/// Represents cached data in json format.
 ///
 ///> Cache is a hardware or software component that stores data so that future
 ///> requests for that data can be served faster; the data stored in a cache
@@ -9,15 +9,15 @@ abstract class JsonCache {
   /// Frees up storage space.
   Future<void> clear();
 
+  /// Removes cached data located at [key].
+  Future<void> remove(String key);
+
+  /// Retrieves cached data located at [key] or null if a cache miss occurs.
+  Future<Map<String, dynamic>?> value(String key);
+
   /// It either updates the data found at [key] with [value] or, if there is no
-  /// previous data at [key], creates a new cache row at [key] with [value].
+  /// previous data at [key], creates a new cache line at [key] with [value].
   ///
   /// **Note**: [value] must be json encodable.
   Future<void> refresh(String key, Map<String, dynamic> value);
-
-  /// Removes data from cache at [key].
-  Future<void> remove(String key);
-
-  /// Retrieves the data value at [key] or null if a cache miss occurs.
-  Future<Map<String, dynamic>?> value(String key);
 }
